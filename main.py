854d4cd0fe00e1b3362007dd37aeb49ff244553d
Markus Säynevirta <25539916+msaynevirta@users.noreply.github.com>
2020-05-16 02:44:16 +0300

    Support code for adding new albums + fixes

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+import csv
+
 from musicalbums import MusicAlbums
 
 def main():
     musicalbums = MusicAlbums("musicalbums.db")
     musicalbums.create_db()
-    print(get_year_of_birth())
+    print(musicalbums.get_year_of_birth())
+
+    filepath = input("Please enter file path for a csv file of new albums: ")
+
+    with(open(filepath, 'r')) as csv_file:
+        csv_reader = csv.reader(csv_file)
+        for row in csv_reader:
+            musicalbums.new_album(row[0], row[1], row[2], row[3], row[4])
 main()
